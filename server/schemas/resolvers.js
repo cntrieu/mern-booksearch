9fee7384f2f9c3cb921d3e3311bbe66e4338d1e9
Calvin Trieu <calvin.trieu@hotmail.com>
2023-05-23 21:56:42 -0400

    completed back-end specifications

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,31 +2,76 @@
 const { User } = require('../models');
 // import sign token function from auth
 const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
     Query: {
-        getSingleUser: async (_, { id, username }, { user }) => {
-            try {
-                let foundUser;
+        // get a single user by either their id or their username
+        me: async (parent, args, context) => {
+            if(context.user) {
+                return User.findOne({ _id: context.user._id})
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        }
+    },
 
-                if(user) {
-                    foundUser = await User.findById(user._id);
-                } else if(id) {
-                    foundUser = await User.findById(id);
-                } else if (username) {
-                    foundUser = await User.findOne({ username });
-                } else {
-                    throw new Error('Invalid. Please provide user id or username')
-                }
+    Mutation: {
+         // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
+        addUser: async (parent, {username, email, password}) => {
+            const user = await User.create({username, email, password});
+            const token = signToken(user);
 
-            if(!foundUser) {
-                throw new Error('Cannot find user with id or username!')
+            return { token, profile };
+        },
+
+         // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
+        // {body} is destructured req.body
+
+        login: async(parent, {email, password}) => {
+            const user = await User.findOne({email})
+
+            if(!user) {
+                throw new AuthenticationError("No user with this email found!")
             }
 
-            return foundUser;
-            } catch (error) {
-                throw new Error('Server error')
+            const correctPw = await user.isCorrectPassword(password)
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password!');
+              }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async(parent, {userId, book}, context) => {
+            if(context.user) {
+                return User.findOneAndUpdate(
+                    { _id: userId},
+                    {
+                        $addToSet: { savedBooks: book}
+                    },
+                    {
+                        new: true,
+                        runValidators: true
+                    }
+                )
             }
+               // If user attempts to execute this mutation and isn't logged in, throw an error
+      throw new AuthenticationError('You need to be logged in!');
+        },
+
+       removeBook: async (parent, {book}, context) => {
+            if(context.user) {
+                return User.findOneAndDelete(
+                    {_id: context.user._id},
+                    { $pull: {savedBooks: book}},
+                    { new: true}
+                    );
+            }
+            throw new AuthenticationError('You need to be logged in!');
         }
     }
 }
+
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+
 const { ApolloServer } = require('apollo-server-express');
 
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: true }));
@@ -22,7 +24,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 const startApolloServer = async () => {
   await server.start();
